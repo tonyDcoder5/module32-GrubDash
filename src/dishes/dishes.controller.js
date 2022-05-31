598aa9d9c777b1ed7842e863c416ea6a2c00d4d7
TonyDCoder5 <tonyaflex5@gmail.com>
2022-05-31 09:24:59 -0600

    initial project upload

---
+++ src/dishes/dishes.controller.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+// Use the existing dishes data
+const dishes = require(path.resolve("src/data/dishes-data"));
+
+// Use this function to assign ID's when necessary
+const nextId = require("../utils/nextId");
+
+// TODO: Implement the /dishes handlers needed to make the tests pass
+const list = (req, res, next) => {
+    res.json({ data: dishes });
+};
+
+
+module.exports = {
+    list,
+    // list: [middleWare, list]
+}
